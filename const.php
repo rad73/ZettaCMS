18d459fb8a47a04664c9905da89341dbc238462d
Александр Хрищанович <dirmax@yandex.ru>
2013-10-22 19:24:12 +0300

    Сделал совместимость с WINDOWS

--- const.php
+++ const.php
@@ -8,13 +8,13 @@ defined('DS')			|| define('DS', DIRECTORY_SEPARATOR);
 defined('ZETTA_MODE')	|| define('ZETTA_MODE', (getenv('ZETTA_MODE') ? getenv('ZETTA_MODE') : 'production'));
 
 defined('SYSTEM_PATH')	|| define('SYSTEM_PATH', realpath(dirname(__FILE__) . '/../zetta'));
-defined('LIBRARY_PATH')	|| define('LIBRARY_PATH', SYSTEM_PATH . '/Library');
-defined('ZETTA_FRAMEWORK_PATH')	|| define('ZETTA_FRAMEWORK_PATH', realpath(SYSTEM_PATH . '/Zetta'));
+defined('LIBRARY_PATH')	|| define('LIBRARY_PATH', realpath(SYSTEM_PATH . DS . 'Library'));
+defined('ZETTA_FRAMEWORK_PATH')	|| define('ZETTA_FRAMEWORK_PATH', realpath(SYSTEM_PATH . DS . 'Zetta'));
 defined('FILE_PATH')	|| define('FILE_PATH', realpath(dirname(__FILE__)));
-defined('TEMP_PATH')	|| define('TEMP_PATH', FILE_PATH . '/Temp');
-defined('MODULES_PATH')	|| define('MODULES_PATH', SYSTEM_PATH . '/Modules');
-defined('HEAP_PATH')	|| define('HEAP_PATH', FILE_PATH . '/Heap');
-defined('USER_FILES_PATH')	|| define('USER_FILES_PATH', FILE_PATH . '/UserFiles');
+defined('TEMP_PATH')	|| define('TEMP_PATH', realpath(FILE_PATH . DS . 'Temp'));
+defined('MODULES_PATH')	|| define('MODULES_PATH', realpath(SYSTEM_PATH . DS . 'Modules'));
+defined('HEAP_PATH')	|| define('HEAP_PATH', realpath(FILE_PATH . DS . 'Heap'));
+defined('USER_FILES_PATH')	|| define('USER_FILES_PATH', realpath(FILE_PATH . DS . 'UserFiles'));
 if (!defined('HTTP_HOST') && array_key_exists('HTTP_HOST', $_SERVER)) {
 	define('HTTP_HOST', 'http://' . $_SERVER['HTTP_HOST']);
 }
