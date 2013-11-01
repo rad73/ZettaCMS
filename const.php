b871de044d21855cc1ef2575c03686895d144863
Александр Хрищанович <dirmax@yandex.ru>
2013-11-01 18:07:43 +0300

    Перенёс логику вычисления HTTP_HOST и REQUEST_URL в const.php и Bootstrap

--- const.php
+++ const.php
@@ -15,8 +15,23 @@ defined('TEMP_PATH')	|| define('TEMP_PATH', realpath(FILE_PATH . DS . 'Temp'));
 defined('MODULES_PATH')	|| define('MODULES_PATH', realpath(SYSTEM_PATH . DS . 'Modules'));
 defined('HEAP_PATH')	|| define('HEAP_PATH', realpath(FILE_PATH . DS . 'Heap'));
 defined('USER_FILES_PATH')	|| define('USER_FILES_PATH', realpath(FILE_PATH . DS . 'UserFiles'));
-if (!defined('HTTP_HOST') && array_key_exists('HTTP_HOST', $_SERVER)) {
-	define('HTTP_HOST', 'http://' . $_SERVER['HTTP_HOST']);
+
+if (!defined('HTTP_HOST')) {	// вычисляем HTTP_HOST
+	
+	if (array_key_exists('HTTP_HOST', $_SERVER)) {
+		define('HTTP_HOST', 'http://' . $_SERVER['HTTP_HOST']);
+	}
+	else {
+		$arrayInput = getopt(false, array('host:'));
+		if (array_key_exists('host', $arrayInput)) {
+			define('HTTP_HOST', 'http://' . $arrayInput['host']);
+		}
+		else {
+			throw new Exception('HTTP_HOST not defined');
+		}
+	}
+
 }
+
 
 set_include_path(SYSTEM_PATH . PATH_SEPARATOR . LIBRARY_PATH . PATH_SEPARATOR . get_include_path());
