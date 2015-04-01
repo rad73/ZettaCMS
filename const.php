75c730a4e4a221ac58619f016d17e7cd005e5384
root <root@www.playontv.net>
2015-04-01 23:04:13 +0300

    Stable release 1.0

--- const.php
+++ const.php
@@ -8,13 +8,14 @@ defined('DS')			|| define('DS', DIRECTORY_SEPARATOR);
 defined('ZETTA_MODE')	|| define('ZETTA_MODE', (getenv('ZETTA_MODE') ? getenv('ZETTA_MODE') : 'production'));
 
 defined('SYSTEM_PATH')	|| define('SYSTEM_PATH', realpath(dirname(__FILE__) . '/../zetta'));
-defined('LIBRARY_PATH')	|| define('LIBRARY_PATH', realpath(SYSTEM_PATH . DS . 'Library'));
 defined('ZETTA_FRAMEWORK_PATH')	|| define('ZETTA_FRAMEWORK_PATH', realpath(SYSTEM_PATH . DS . 'Zetta'));
 defined('FILE_PATH')	|| define('FILE_PATH', realpath(dirname(__FILE__)));
 defined('TEMP_PATH')	|| define('TEMP_PATH', realpath(FILE_PATH . DS . 'Temp'));
 defined('MODULES_PATH')	|| define('MODULES_PATH', realpath(SYSTEM_PATH . DS . 'Modules'));
 defined('HEAP_PATH')	|| define('HEAP_PATH', realpath(FILE_PATH . DS . 'Heap'));
 defined('USER_FILES_PATH')	|| define('USER_FILES_PATH', realpath(FILE_PATH . DS . 'UserFiles'));
+defined('LIBRARY_PATH')	|| define('LIBRARY_PATH', realpath(SYSTEM_PATH . DS . 'Library'));
+defined('USER_LIBRARY_PATH')	|| define('USER_LIBRARY_PATH', realpath(FILE_PATH . DS . 'Library'));
 
 if (!defined('HTTP_HOST')) {	// вычисляем HTTP_HOST
 	
@@ -34,4 +35,4 @@ if (!defined('HTTP_HOST')) {	// вычисляем HTTP_HOST
 }
 
 
-set_include_path(SYSTEM_PATH . PATH_SEPARATOR . LIBRARY_PATH . PATH_SEPARATOR . get_include_path());
+set_include_path(SYSTEM_PATH . PATH_SEPARATOR . LIBRARY_PATH . PATH_SEPARATOR . USER_LIBRARY_PATH . PATH_SEPARATOR . get_include_path());
