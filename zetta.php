1b1698ab2dd67fa2a3b2ec880449d069b4f908e3
Александр Хрищанович <dirmax@yandex.ru>
2016-12-13 11:27:23 +0300

    Clean _GLOBALS var

--- zetta.php
+++ zetta.php
@@ -6,18 +6,16 @@ require_once 'autoloader.php';
 /* Обрабатываем все ошибки включая фатальные */
 register_shutdown_function(array(new Zetta_ErrorHandler(), 'error'));
 
-/* Включаем конфигурационные файлы */
-$application = new Zend_Application(ZETTA_MODE, array('config' => array_merge(
-	glob(SYSTEM_PATH . '/Configs/_*.ini', GLOB_NOSORT),
-	array(
-		FILE_PATH . '/Configs/application.ini',
-		SYSTEM_PATH . '/Configs/application.ini',
-	)
-)));
-
 try {
 
-	$application
+	/* Включаем конфигурационные файлы и стартуем */
+	(new Zend_Application(ZETTA_MODE, array('config' => array_merge(
+		glob(SYSTEM_PATH . '/Configs/_*.ini', GLOB_NOSORT),
+		array(
+			FILE_PATH . '/Configs/application.ini',
+			SYSTEM_PATH . '/Configs/application.ini',
+		)
+	))))
 		->bootstrap()
 		->run();
 
--- zetta_quick.php
+++ zetta_quick.php
@@ -9,19 +9,19 @@ require_once 'autoloader.php';
 /* Обрабатываем все ошибки включая фатальные */
 register_shutdown_function(array(new Zetta_ErrorHandler(), 'error'));
 
-/* Включаем конфигурационные файлы */
-$application = new Zend_Application(ZETTA_MODE, array('config' => array(
-	SYSTEM_PATH . '/Configs/_autoloader.ini',
-	SYSTEM_PATH . '/Configs/_log.ini',
-	SYSTEM_PATH . '/Configs/_router.ini',
-	SYSTEM_PATH . '/Configs/_view.ini',
-	SYSTEM_PATH . '/Configs/_frontcontroller.ini',
-	SYSTEM_PATH . '/Configs/application_quick.ini',
-)));
-
 try {
-	$application
+	
+	/* Включаем конфигурационные файлы и стартуем */
+	(new Zend_Application(ZETTA_MODE, array('config' => array(
+		SYSTEM_PATH . '/Configs/_autoloader.ini',
+		SYSTEM_PATH . '/Configs/_log.ini',
+		SYSTEM_PATH . '/Configs/_router.ini',
+		SYSTEM_PATH . '/Configs/_view.ini',
+		SYSTEM_PATH . '/Configs/_frontcontroller.ini',
+		SYSTEM_PATH . '/Configs/application_quick.ini',
+	))))
 		->bootstrap()
 		->run();
+
 }
 catch (Exception $e) { }
